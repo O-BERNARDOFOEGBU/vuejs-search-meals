cfb9263bcd3dad20ca492313661697ce138b0e7e
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-10-11 23:56:32 +0100

    default layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<Template>
+   <header class="bg-white shadow-md h-16 flex justify-between items-stretch">
+      <router-link
+        :to="{ name: 'home' }"
+        class="inline-flex items-center px-5 h-full"
+      >
+        Home
+      </router-link>
+      <div class="flex items-center gap-1">
+        <router-link
+          :to="{ name: 'byName' }"
+          class="inline-flex items-center px-2 h-full transistion-colors hover:bg-purple-100"
+        >
+          Search Meals
+        </router-link>
+        <router-link
+          :to="{ name: 'byLetter' }"
+          class="inline-flex items-center px-2 h-full transistion-colors hover:bg-purple-100"
+        >
+          Meals By letter
+        </router-link>
+        <router-link
+          :to="{ name: 'byIngredient' }"
+          class="inline-flex items-center px-2 h-full transistion-colors hover:bg-purple-100"
+        >
+          Meals By Ingredients
+        </router-link>
+      </div>
+    </header>
+</Template>
